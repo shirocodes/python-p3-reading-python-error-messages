9192a6fe16fbabc2819f45b1efbf13e9e3e1d6ca
shirocodes <ishirapt@gmail.com>
2025-05-06 13:37:57 +0300

    Completed the lab

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
-
+hello_world = "Hello World! Pass this test, please."
 print(hello_world)
--- lib/a_syntax_error.py
+++ lib/a_syntax_error.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 
-poor_syntax = 2 * #
+poor_syntax = 2 * 3 # Omiting 3 is an example of syntax error
